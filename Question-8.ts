832b1085787f1295edd6514c0151b0cbba6714ce
mohitchawla9 <63363623+mohitchawla9@users.noreply.github.com>
2020-04-09 15:29:13 +0530

    Assignment 1 Question 8 solution
    Solution to Question 8 i.e to find subarray of evensorted number

---
+++ Question-8.ts
@@ -0,0 +1,15 @@
+function getEvenSort(numarr:number[]){
+    let newarr = new Array();
+    for(var i = 0; i < numarr.length; i++){ 
+       if(numarr[i] % 2 === 0){
+         newarr.push(numarr[i]);  
+       }
+    }
+    newarr = newarr.sort((n1,n2) => n1 - n2);
+    for(var i = 0; i < newarr.length; i++)
+    { 
+      console.log(newarr[i]);
+    }
+}
+let numarr: number[] = [2,4,1,88,68,48,5,6,8,9];
+getEvenSort(numarr);
